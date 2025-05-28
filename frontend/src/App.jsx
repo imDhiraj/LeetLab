e323fb8f05c2c02bc844c34ba0d7b1f228696c64
Dhiraj Kumbhar <93609511+imDhiraj@users.noreply.github.com>
2025-05-28 12:24:30 +0530

    Started with Frontend & signup/login

---
+++ frontend/src/App.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from 'react'
+import {Route , Routes , Navigate } from  "react-router-dom"
+import {Toaster} from "react-hot-toast"
+
+
+
+import HomePage from './page/HomePage'
+import LoginPage from './page/LoginPage'
+import SignUpPage from './page/SignUpPage'
+import { useAuthStore } from './store/useAuthStore'
+import { Loader } from 'lucide-react'
+
+const App = () => {
+
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+
+  useEffect(()=>{
+checkAuth()
+  },[checkAuth])
+
+  if (isCheckingAuth && !authUser){
+    return(
+      <div className = "flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin"/>
+      </div>
+    )
+  }
+    return (
+      <div className="flex flex-col items-center justify-start">
+        <Toaster />
+        <Routes>
+          <Route
+            path="/"
+            element={authUser ? <HomePage /> : <Navigate to={"/login"} />}
+          />
+
+          <Route
+            path="/login"
+            element={!authUser ? <LoginPage /> : <Navigate to={"/"} />}
+          />
+
+          <Route
+            path="/SignUp"
+            element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />}
+          />
+        </Routes>
+      </div>
+    );
+}
+
+export default App
